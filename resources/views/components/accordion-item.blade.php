76682268983d5c1e70d287b1f149765bb194280a
Marvellous <ifezueyoung@gmail.com>
2024-01-06 09:44:47 -0800

    accordion wip

--- resources/views/components/accordion-item.blade.php
+++ resources/views/components/accordion-item.blade.php
@@ -1,14 +1,14 @@
 <div x-data="{open: false, index: (Math.random() + 1).toString(36).substring(7)" x-effect="active != index ? open = false : open = true" class="w-full">
     <div :class="{'w-full', 'my-3 rounded' : groupTogether == false, 'border-b border-x border-gray-300' : groupTogether == true}" @click="open = true; active = index" :class="{'active:ring-4 active:ring-gray-400' : open}">
         <div :class="'flex justify-between p-4','rounded-t-lg' : groupTogether == true, 'bg-gray-100 border-b cursor' : open, 'cursor-pointer' : open == false}">
-            <h3 class="@isset($item['title_class']) {{$item['title_class']}} @endisset  text-2xl">{!!$item['title']!!}</h3>
+            <h3 class="text-2xl">{{$title}}</h3>
             <button x-text="open == false ? '+' : '-'" class="text-2xl"></button>
         </div>
         <div x-show="open"
         x-transition:enter="transition ease-linear  duration-100"
         x-transition:enter-start=" height-0"
         x-transition:enter-end=""
-        class="@isset($item['body_class']) {{$item['body_class']}} @endisset {{$bodyClass}} p-4 text-gray-500">
+        class=" p-4 text-gray-500">
             {{$slot}}
         </div>
     </div>
