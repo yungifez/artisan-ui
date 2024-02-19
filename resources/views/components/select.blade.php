0fee48c1db1b86d027490ed99719dc731fb40f92
Marvellous <ifezueyoung@gmail.com>
2024-02-18 23:41:18 -0800

    wip

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -87,7 +87,7 @@
                                 <div class="flex flex-auto flex-wrap">
                                     {{--display selected items--}}
                                     <template x-for="(option,index) in selected" :key="options[option].value">
-                                        <div class="flex justify-center dark:text-gray-200 items-center font-medium px-2 rounded-full text-inherit bg-inherit border border-gray-400 ">
+                                        <div class="flex justify-center dark:text-gray-200 items-center font-medium p-2 rounded-full text-inherit bg-inherit border border-gray-400 ">
                                             <div class="text-xs font-normal leading-none max-w-full flex-initial" x-model="options[option]" x-text="options[option].text"></div>
                                             <div class=" px-1 flex flex-auto flex-row-reverse">
                                                 <button x-on:click="remove(index,option)" class="">
@@ -98,22 +98,17 @@
                                     </template>
                                     {{--display when no items are selected --}}
                                     <div x-show="selected.length  == 0" class="flex-1 justify-self-center" >
-                                        <p class="bg-transparent cursor-default pr-2 appearance-none outline-none h-full w-full text-gray-800 dark:text-gray-400" :value="selectedValues()">
+                                       <p class="bg-transparent cursor-default pr-2 appearance-none outline-none h-full w-full text-gray-800 dark:text-gray-400" :value="selectedValues()">
                                             {{$attributes->get('placeholder') ?? "Select Options"}}
                                         </p>
                                     </div>
                                 </div>
-                            <div class="text-gray-300 w-8 pl-4 border-l flex items-center border-gray-400 ">
-                                <button type="button" x-show="isOpen() === true" x-on:click="open" class="cursor-pointer w-6 h-6 text-[var(--primary-text-color)] dark:text-[var(--dark-primary-text-color)] fill-[var(--primary-text-color)] dark:fill-[var(--dark-primary-text-color)] outline-none focus:outline-none" >
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" class="w-8/12 text-center" version="1.1"><path d="M903.232 256l56.768 50.432L512 768 64 306.432 120.768 256 512 659.072z"/></svg>
-                                        <path id="XMLID_224_" d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394  l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393  C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"/>
-                                        </svg>
-                                </button>
-                                <button type="button" x-show="isOpen() === false" @click="close" class="cursor-pointer w-6 h-6 text-gray-600 outline-none dark:fill-gray-200  focus:outline-none">
-                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="w-8/12 text-center" version="1.1" id="Layer_1" viewBox="0 0 330 330" xml:space="preserve">
-                                        <path id="XMLID_224_" d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394  l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393  C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"/>
+                            <div class="border-[color:var(--select-border-color)] dark:border-[color:var(--select-dark-border-color)]  w-8 pl-4 border-l flex items-center  ">
+                                <div class="cursor-pointer text-[var(--primary-text-color)] dark:text-[var(--dark-primary-text-color)] fill-[var(--primary-text-color)] dark:fill-[var(--dark-primary-text-color)] " >
+                                    <svg :class="{'rotate-180' : isOpen()}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" class="w-8/12 transition text-center" version="1.1">
+                                        <path d="M903.232 256l56.768 50.432L512 768 64 306.432 120.768 256 512 659.072z"/>
                                     </svg>
-                                </button>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -122,10 +117,10 @@
                             <div class="flex flex-col w-full">
                                 <template x-for="(option,index) in options" :key="index">
                                     <div>
-                                        <div class="cursor-pointer w-full border-gray-100 rounded-t hover:bg-gray-500 dark:hover:bg-white dark:hover:bg-opacity-10 hover:text-white bg-opacity-25" @click="select(index,$event)">
-                                            <div :class="(option.selected) ? 'bg-gray-400 dark:bg-neutral-800 text-white' : ''" class="flex w-full items-center py-1 relative">
+                                        <div class="cursor-pointer w-full border-gray-100 rounded-t hover:bg-neutral-300 dark:hover:bg-white dark:hover:bg-opacity-10 hover:text-white bg-opacity-25" @click="select(index,$event)">
+                                            <div :class="(option.selected) ? 'bg-neutral-400 dark:bg-neutral-800 text-white' : ''" class="flex w-full items-center py-1 relative">
                                                 <div class="w-full items-center flex">
-                                                    <div class="mx-2 leading-6" x-model="option" x-text="option.text"></div>
+                                                    <div class="ml-3 leading-6 text-sm" x-model="option" x-text="option.text"></div>
                                                 </div>
                                             </div>
                                         </div>
