3c42f4a10855e57e52dc272a1a15b4570a8573ac
Marvellous <ifezueyoung@gmail.com>
2024-04-10 12:58:44 -0700

    Add event listeners to popover

--- resources/views/components/popover.blade.php
+++ resources/views/components/popover.blade.php
@@ -15,6 +15,7 @@
     <div
         class="w-fit inline-block"
         @click="toggle()"
+        @keydown.escape="close()"
         x-ref="trigger"
     >
         @isset($popoverTrigger)
@@ -24,7 +25,7 @@
         @endisset
     </div>
     @isset($popoverContent)
-        <div x-anchor.offset.4="$refs.trigger" x-show="popover" x-transition {{$popoverContent->attributes->class(["z-50 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none"])}}>
+        <div x-anchor.offset.4="$refs.trigger" x-trap="popover" @click.outside="close()" x-show="popover" x-transition {{$popoverContent->attributes->class(["rounded-md border bg-popover text-popover-foreground shadow-md outline-none"])}}>
             {{$popoverContent}}
         </div>
     @endisset
