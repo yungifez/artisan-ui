5d3a2b5388ca785c84013095bc5f939acafd764a
Marvellous <ifezueyoung@gmail.com>
2024-04-15 21:58:24 -0700

    Add seperator

---
+++ resources/views/components/separator.blade.php
@@ -0,0 +1,4 @@
+@props(['orientation' => 'horizontal'])
+
+<div data-orientation="{{$orientation}}" role="none" {{$attributes->class(['shrink-0 bg-border w-full my-4', 'h-[1px]
+    w-full' => $orientation == 'horizontal', 'h-[1px] w-full' => $orientation == 'vertical'])}}></div>
