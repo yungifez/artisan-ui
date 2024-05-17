61a7a4d41b625cbae3d15c69373bf0210b501c36
Marvellous <ifezueyoung@gmail.com>
2024-05-16 18:21:01 -0700

    Emergency commit

--- resources/views/components/avatar.blade.php
+++ resources/views/components/avatar.blade.php
@@ -1,32 +1,28 @@
 @props([
-    'class' => '',
-    'fallback',
-    'borderRadius' => "rounded-full",
+'class' => '',
+'fallback',
+'borderRadius' => "rounded-full",
 ])
 
 @php
-    $class .= " ".match($attributes->get("size")){
-        default => "h-10 w-10",
-        'sm' => "h-8 w-8",
-        'lg' => "h-16 w-16",
-        'none' => "",
-    }
+$class .= " ".match($attributes->get("size")){
+default => "h-10 w-10",
+'sm' => "h-8 w-8",
+'lg' => "h-16 w-16",
+'none' => "",
+}
 @endphp
 
-<div x-data="avatar()" {{$attributes->class(["$class $borderRadius aspect-square border flex justify-center items-center"])}}>
+<div x-data="avatar()" {{$attributes->class(["$class $borderRadius aspect-square border flex justify-center
+    items-center"])}}>
     @isset($image)
-        <img
-            x-bind="image"
-            {{$image->attributes->class(["$borderRadius"])}}
-        >
+    <img x-bind="image" {{$image->attributes->class(["$borderRadius"])}}
+    >
     @endisset
     @isset($fallback)
-        <div
-            x-show="loadError"
-            x-bind="fallback"
-            {{$fallback->attributes->class(['p-3'])}}
+    <div x-bind="fallback" {{$fallback->attributes->class(['p-3'])}}
         >
-            {{$fallback}}
-        </div>
+        {{$fallback}}
+    </div>
     @endisset
 </div>
