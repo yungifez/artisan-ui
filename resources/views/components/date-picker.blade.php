c8ec0ef8d60b72aa24f26bd2593b9c6493ba1307
Marvellous <ifezueyoung@gmail.com>
2024-06-20 15:50:14 -0700

    Fix chaos I created

--- resources/views/components/date-picker.blade.php
+++ resources/views/components/date-picker.blade.php
@@ -23,7 +23,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
             </svg>
-            <template x-if="mode != 'single'">
+            <template x-if="mode != 'multiple' && mode != 'range'">
                 <span>
                     <span x-text="value == null ? null : formatDate(value)"></span>
                     <input type="hidden" {{$attributes}} :value="value" />
