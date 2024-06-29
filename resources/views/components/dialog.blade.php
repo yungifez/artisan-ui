c767b18c62d8f5c810a7f5dc79f7b51158504b28
Marvellous <ifezueyoung@gmail.com>
2024-06-29 15:56:51 -0700

    Add teleport support to dialog

--- resources/views/components/dialog.blade.php
+++ resources/views/components/dialog.blade.php
@@ -6,14 +6,17 @@
         @endisset
     </div>
 
-    <div {{$attributes->class(["fixed min-h-[100vh] inset-0 z-50 bg-black/80" ])}} x-bind="overlay">
-        @isset($content)
-        <div x-bind="dialog" {{$content->attributes->class(["fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg
-            translate-x-[-50%]
-            translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 sm:rounded-lg"])}}
-            >
-            {{$content}}
+    <template @if ($attributes->has('x-teleport')) x-teleport="{{$attribute->get('x-teleport')}}" @else x-if="true"
+        @endif>
+        <div {{$attributes->class(["fixed min-h-[100vh] inset-0 z-50 bg-black/80" ])}} x-bind="overlay">
+            @isset($content)
+            <div x-bind="dialog" {{$content->attributes->class(["fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg
+                translate-x-[-50%]
+                translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 sm:rounded-lg"])}}
+                >
+                {{$content}}
+            </div>
+            @endisset
         </div>
-        @endisset
-    </div>
+    </template>
 </div>
