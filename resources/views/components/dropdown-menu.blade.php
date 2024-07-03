e96318cb3a170c54499863142ce11f164490ffd8
Marvellous <ifezueyoung@gmail.com>
2024-07-03 11:37:27 -0700

    More dropdown changes

--- resources/views/components/dropdown-menu.blade.php
+++ resources/views/components/dropdown-menu.blade.php
@@ -1,12 +1,10 @@
 @props(['trigger', 'content'])
 <div x-data="dropdownMenu">
-    <div class="w-fit inline-block" x-bind="trigger" x-ref="trigger">
-        @isset($trigger)
+    @isset($trigger)
+    <div {{$trigger->attributes->class(['w-fit inline-block'])}} x-bind="trigger" x-ref="trigger">
         {{$trigger}}
-        @else
-        <x-aui::button variant="outline" type="button">Open</x-aui::button>
-        @endisset
     </div>
+    @endisset
     @isset($content)
     <div x-bind="content" x-cloak {{$content->attributes->class(["z-50 min-w-[8rem] rounded-md border
         bg-popover p-1
