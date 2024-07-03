4cbbf8535aaf11b6655d25b52622b1c9f0634712
Marvellous <ifezueyoung@gmail.com>
2024-07-03 12:29:53 -0700

    Fix bug

--- resources/views/components/dropdown-menu-item.blade.php
+++ resources/views/components/dropdown-menu-item.blade.php
@@ -1,7 +1,7 @@
 <div>
     <x-aui::button :attributes="$attributes->class(['relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm
         outline-none transition-colors focus:bg-accent focus:text-accent-foreground w-full'])" role="menu-item"
-        x-bind="menuItem" variant="ghost">
+        size="sm" x-bind="menuItem" variant="ghost">
         {{$slot}}
     </x-aui::button>
 </div>
