f2e73e6efa7a09477b8bcdaa68ca827be6b09b42
Marvellous <ifezueyoung@gmail.com>
2024-07-22 18:28:15 -0700

    Add text decoration none

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -1,5 +1,5 @@
 @php
-$class = "inline-flex items-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background
+$class = "inline-flex items-center whitespace-nowrap no-underline rounded-md text-sm font-medium ring-offset-background
 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2
 disabled:pointer-events-none disabled:opacity-50 ";
 
