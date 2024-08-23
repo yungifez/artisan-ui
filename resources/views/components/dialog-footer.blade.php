d81aba5c8769b618c663831ea198a81af4e79e4b
Marvellous <ifezueyoung@gmail.com>
2024-08-22 22:08:26 -0700

    Add fill to svg

--- resources/views/components/dialog-footer.blade.php
+++ resources/views/components/dialog-footer.blade.php
@@ -8,7 +8,7 @@
             @else
             <button type="button"
                 class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none ">
-                <x-aui::x />
+                <x-aui::x class="fill-foreground" />
                 <span class="sr-only">Close</span>
             </button>
             @endisset
