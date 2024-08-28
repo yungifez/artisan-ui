a124d07ef03ad8dc1adddab9e43daac1ca0f84df
Marvellous <ifezueyoung@gmail.com>
2024-08-28 12:27:24 -0700

    Add manifest to script url

--- src/Handlers/FrontendAssetsHandler.php
+++ src/Handlers/FrontendAssetsHandler.php
@@ -8,26 +8,31 @@ class FrontendAssetsHandler
 {
     public function boot()
     {
-        Blade::directive('artisanStyles', function () {
+        $distPath = __DIR__.'/../../dist/';
+        $manifest = file_get_contents($distPath.'manifest.json');
+        $manifestHashes = json_decode($manifest, true);
+
+        Blade::directive('artisanStyles', function () use ($manifestHashes) {
             $html = '';
             if (config('app.debug') == 'true') {
-                $html = "<?php echo '<link rel=\"stylesheet\" href=\"".route('artisan-ui.artisan.css')."\">' ; ?>";
+                $html = "<?php echo '<link rel=\"stylesheet\" href=\"".route('artisan-ui.artisan.css').'?ver='.$manifestHashes['/artisan.css']."\">' ; ?>";
             } else {
-                $html = "<?php echo '<link rel=\"stylesheet\" href=\"".route('artisan-ui.artisan.min.css')."\">' ; ?>";
+                $html = "<?php echo '<link rel=\"stylesheet\" href=\"".route('artisan-ui.artisan.min.css').'?ver='.$manifestHashes['/artisan.css']."\">' ; ?>";
             }
 
             return $html;
         });
 
-        Blade::directive('artisanScripts', function () {
+        Blade::directive('artisanScripts', function () use ($manifestHashes)  {
             $html = '';
             if (config('app.debug') == 'true') {
-                $html = "<?php echo '<script src=\"".route('artisan-ui.artisan.js')."\"></script>' ; ?>";
+                $html = "<?php echo '<script src=\"".route('artisan-ui.artisan.js').'?ver='.$manifestHashes['/artisan.js']."\"></script>' ; ?>";
             } else {
-                $html = "<?php echo '<script src=\"".route('artisan-ui.artisan.min.js')."\"></script>' ; ?>";
+                $html = "<?php echo '<script src=\"".route('artisan-ui.artisan.min.js').'?ver='.$manifestHashes['/artisan.js']."\"></script>' ; ?>";
             }
 
             return $html;
         });
+
     }
 }
