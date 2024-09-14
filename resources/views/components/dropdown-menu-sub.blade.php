b511d3a6ddcc18544d226b47d5b8b114ba38e4ba
Marvellous <ifezueyoung@gmail.com>
2024-09-14 13:46:20 -0700

    Resolves #11

--- resources/views/components/dropdown-menu-sub.blade.php
+++ resources/views/components/dropdown-menu-sub.blade.php
@@ -1,6 +1,6 @@
 <div x-data="dropdownMenuSub" x-bind="root" role="menuitem" aria-haspopup="menu" {{$attributes}}>
     @isset($trigger)
-    <x-aui::button variant="ghost" type="button" x-ref="subTrigger" tabindex="-1" x-bind="trigger"
+    <x-aui::button variant="ghost" size="none" type="button" x-ref="subTrigger" tabindex="-1" x-bind="trigger"
         ::class="{'bg-accent' : subOpen}" :attributes="$trigger->attributes->class(['flex w-full cursor-default
         select-none items-center rounded-sm
         px-2 py-1.5 text-sm outline-none focus:bg-accent'])">
