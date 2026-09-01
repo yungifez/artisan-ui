baa9a99681d620c09436b0f740f6e635e9df1087
Marvellous <ifezueyoung@gmail.com>
2026-08-31 21:08:00 -0700

    Fix calendar preview event scope

--- docs/previews/calendar-multiple-max-demo.blade.php
+++ docs/previews/calendar-multiple-max-demo.blade.php
@@ -1,6 +1,6 @@
-<div x-data="{ value: [] }" class="flex max-w-72 flex-col items-center">
+<div x-data="{ value: [] }" @value-change="value = $event.detail.value" class="flex max-w-72 flex-col items-center">
     <april:calendar class="rounded-md border" mode="multiple" max="5"
         :selected="[now(), now()->addDays(2), now()->addDays(14)]"
-        @value-change="value = $event.detail.value" />
+        />
     <p x-text="'Selected dates are '+value.toString()" class="my-2 wrap-break-word"></p>
 </div>
--- docs/previews/calendar-range-max-min-demo.blade.php
+++ docs/previews/calendar-range-max-min-demo.blade.php
@@ -1,5 +1,5 @@
-<div x-data="{ value: { from: null, to: null } }" class="flex max-w-72 flex-col items-center">
+<div x-data="{ value: { from: null, to: null } }" @value-change="value = $event.detail.value" class="flex max-w-72 flex-col items-center">
     <april:calendar class="rounded-md border" mode="range" :selected="['from' => now(), 'to' => now()->addDays(14)]"
-        max="15" min="6" @value-change="value = $event.detail.value" />
+        max="15" min="6" />
     <p x-text="'Selected range is from '+value['from']+' to '+value['to']" class="my-2 wrap-break-word"></p>
 </div>
