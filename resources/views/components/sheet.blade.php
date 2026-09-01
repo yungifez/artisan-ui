1398ffd2087b3c5698c6d672adbd7bc2b4736d12
Marvellous <ifezueyoung@gmail.com>
2026-08-31 22:49:29 -0700

    Make bottom sheets full width

--- resources/views/components/sheet.blade.php
+++ resources/views/components/sheet.blade.php
@@ -6,7 +6,7 @@ if (isset($content)){
 $class .= ' '.match($content->attributes->get("side")){
 default => "inset-y-0 right-0 h-full w-3/4 border-l sm:max-w-sm",
 'top' => "inset-x-0 w-full top-0 border-b",
-'bottom' => "inset-x-0 bottom-0 border-t",
+'bottom' => "inset-x-0 w-full bottom-0 border-t",
 'left' => "inset-y-0 left-0 h-full w-3/4 border-r sm:max-w-sm",
 'right' => "inset-y-0 right-0 h-full w-3/4 border-l sm:max-w-sm",
 "none" => "",
